30e22b3be60788d92ec65c6e7bf49b2ad1c53298
kilrizzy <jeffkilroy@gmail.com>
2021-06-24 18:26:32 -0400

    supply

--- contracts/D20Token.sol
+++ contracts/D20Token.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract D20Token is ERC20 {
+    
+    // Maximum total supply of the token (20M)
+    uint256 private _maxTotalSupply = 20000000000000000000000000;
+
     constructor() ERC20("D20Token", "D20") {
+        mint(_maxTotalSupply);
     }
+
+    /**
+     * @dev Creates `amount` tokens and assigns them to `msg.sender`, increasing
+     * the total supply.
+     */
+    function mint(uint256 amount) public returns (bool) {
+        require(totalSupply() + amount <= _maxTotalSupply, "ERC20: minting more then MaxTotalSupply");
+        _mint(_msgSender(), amount);
+        return true;
+    }
+
 }
--- truffle-config.js
+++ truffle-config.js
@@ -36,6 +36,9 @@ module.exports = {
    */
 
   networks: {
+    ftmTestnet: {
+      network_id: "4002",       // Any network (default: none)
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
