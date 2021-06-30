ac2d0990b361d9dee76bc109a07359285b3c8f5a
kilrizzy <jeffkilroy@gmail.com>
2021-06-30 18:49:53 -0400

    add withdrawaltowallet

--- contracts/D20Token.sol
+++ contracts/D20Token.sol
@@ -5,10 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract D20Token is ERC20, ERC20Burnable {
+    // Owner wallet
+    address payable wallet;
     // Maximum total supply of the token (20M)
     uint256 private _maxTotalSupply = 20000000000000000000000000;
 
     constructor() ERC20("D20Token", "D20") {
+        wallet = payable(msg.sender);
         mint(_maxTotalSupply);
     }
 
@@ -20,5 +23,11 @@ contract D20Token is ERC20, ERC20Burnable {
 
         _mint(_msgSender(), amount);
         return true;
+    }
+
+    function withdrawalToWallet() public payable {
+        require(msg.sender == wallet);
+        address(this).balance;
+        wallet.transfer(address(this).balance);
     }
 }
